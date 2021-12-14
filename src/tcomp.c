d463f01214bb648e49aab9d15445d28711721757
Axel <gabirodz03@gmail.com>
2021-12-14 17:10:03 -0600

    update tcomp test

--- src/tcomp.c
+++ src/tcomp.c
@@ -3,18 +3,21 @@
 #include "hufftree.h"
 #include "charfreqnode.h"
 #include "codetable.h"
+#include "strbin.h"
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
         return 2;
 
-    char const *path = argv[1];
+    char const *src_path = argv[1];
+    char const *dest_path = argv[2];
 
-    FILE *fp = fopen(path, "r");
+    FILE *src_fp = fopen(src_path, "r");
 
-    HuffTree t = huffTree_construct_f_stream(fp);
+    HuffTree t = huffTree_construct_f_stream(src_fp);
 
+    fclose(src_fp);
     huffTree_qsort(&t);
     puts("freqtable--------");
     huffTree_print_table(t);
@@ -22,6 +25,7 @@ int main(int argc, char const *argv[])
     puts("codetable------------");
     CodeTable c_t = construct_CodeTable_f_tree(t);
     codeTable_print(c_t);
+    write_encode_to_file(src_path, dest_path, c_t);
     huffTree_destroy(t);
 
     return 0;
